c697ac1bb638730202277c1882fcbf80d165b9c5
cbr201408 <32245920+cbr201408@users.noreply.github.com>
2018-04-07 10:20:52 -0500

    Delete config.py

--- config.py
+++
@@ -1,5 +0,0 @@
-# Twitter API Keys
-consumer_key = "Z69lDk87UGAq3aTAr0PFWgc4j"
-consumer_secret = "qZuDWaiI3f7S7Eu1LmUMyWRLRkOxKU37ngWhYeFApkCKvXQ8sm"
-access_token = "895676768836739072-C1hkFGDYNJaVrGkNUpgiMHmXShQIt8a"
-access_token_secret = "QHdwKJsk2SGqPJKegAxmz61pUSAz2VifGVMvKLesEL11l"
